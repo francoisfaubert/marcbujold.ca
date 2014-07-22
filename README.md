092c6fe3f3fa4664e5806f772768ab07c0648767
François Faubert <francois.faubert@iprospect.com>
2014-07-22 14:50:40 -0400

    Cant send emails from my EC2, only leave the phone number for now

---
+++ README.md
@@ -0,0 +1,3 @@
+## Marc Bujold.ca
+
+C'est le site du cousin a ta mere, il ne sais pas que c'est toi qui paye le .ca
---
+++ contact/index.php
@@ -0,0 +1,7 @@
+<?php
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    // tant que jai un probleme avec mes email sur EC2, on peut pas envoyer de email
+}
+
+
